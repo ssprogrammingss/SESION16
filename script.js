7bf589b97703de623fe5e5608a127b24b72e0d4a
ssprogrammingss <bryant.munoz.ttv.975@unilibre.edu.co>
2024-10-23 00:30:25 -0500

    corregir form

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function validarFormulario(){
     let nombre=document.getElementById("nombre").Value;
-    let nombre=document.getElementById("email").Value;
-    let nombre=document.getElementById("imagen").Value;
+    let email=document.getElementById("email").Value;
+    let imagen=document.getElementById("imagen").Value;
     if(nombre=="" || email=="" || imagen=="" ){
         alert("Todos los campos son obligatorios...");
         return false;
     }
         return true;
 }
-    function mostrarImagen(event){
+function mostrarImagen(event){
     let imagen=document.getElementById("ver-imagen");
     imagen.src=URL.createObjectURL(event.target.files[0]);
 }
