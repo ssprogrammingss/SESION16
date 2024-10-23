c5a3763cb6fd8edf9efa84eb8c9ef759c5ee543c
ssprogrammingss <bryant.munoz.ttv.975@unilibre.edu.co>
2024-10-23 01:39:21 -0500

    img

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ function validarFormulario(){
     let email=document.getElementById("email").Value;
     let imagen=document.getElementById("imagen").Value;
     if(nombre=="" || email=="" || imagen=="" ){
-
         alert("Todos los campos son obligatorios...");
         return false;
     }
     return true;
 }
 function mostrarImagen(event){
-    let imagen=document.getElementById("ver-imagen");
+    let imagen=document.getElementById('ver-imagen');
     imagen.src=URL.createObjectURL(event.target.files[0]);
 }
     
